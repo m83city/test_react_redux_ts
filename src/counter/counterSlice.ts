c2f18893b75ee519a6aafdee882f08c8de34f30c
M83 <rostorguievv@gmail.com>
2022-10-01 18:22:54 +0300

    simple counter

---
+++ src/counter/counterSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+    count: 0
+}
+export const counterSlice = createSlice({
+    name:"counter",
+    initialState,
+    reducers:{
+        increment:(state:any)=>{
+            state.count +=  1
+        },
+        decrement:(state:any)=>{
+            state.count -= 1 
+        }
+
+    }
+})
+export const {increment, decrement} = counterSlice.actions;
+export default counterSlice.reducer;
